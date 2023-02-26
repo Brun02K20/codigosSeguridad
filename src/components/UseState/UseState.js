b1565d00d70c3db5ab54ff649825179459f1e912
Bruno Virinni <bvirinni@gmail.com>
2023-02-26 19:23:25 -0300

    Ajustes de los estados de error y de loading

--- src/components/UseState/UseState.js
+++ src/components/UseState/UseState.js
@@ -1,16 +1,36 @@
 import React from "react";
 
+// al ser todo mayuscula indicamos que nunca el valor almacenado cambiara, es una convencion nada mas
+const SECURITY_CODE = "paradigma";
+
 function UseState({name}){
     // crear un estado en componentes tipos funcion
     // const [state, setState] = React.useState(initialState);
 
-    const [error, setError] = React.useState(true);
+    const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
+
+    // creando un estado dinamico que dependa de lo que escriban los usuarios en un input
+    const [value, setValue] = React.useState('');
+
+    // debuggueando el estado de value
+    console.log(value);
+
 
     React.useEffect(()=>{
         if(!!loading){
+            setError(false);
             setTimeout(() => {
                 console.log("Inicio");
+
+                // validando si la palabra ingresada por el usuario es correcta
+                if (value === SECURITY_CODE) {
+                    setLoading(false);
+                } else {
+                    setError(true);
+                    setLoading(false);
+                };
+
                 setLoading(false);
                 console.log("Fin");
             }, 2000);
@@ -26,10 +46,18 @@ function UseState({name}){
 
             {loading && (<p>LOADING...</p>)}
 
-            <input placeholder="Security code..." />
+            {/* nota: el evento que se recibe de parametro es lo que escriba el usuario */}
+            <input 
+                placeholder="Security code..." 
+                value={value} 
+                onChange={(evento) => {setValue(evento.target.value)}}
+            />
+            
             <button
                 onClick={()=>{setLoading(true)}}
-            >Comprobar</button>
+            >
+                Comprobar
+            </button>
         </div>
     );
 };
